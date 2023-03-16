dc5ffac3ea0f82fd53b13995c6217f5c0936f23d
vivek <vivekanandpv@outlook.com>
2023-03-16 06:33:27 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/SpringBootRestfulWebservicesStudyApplication.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootrestfulwebservicesstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWebservicesStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestfulWebservicesStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vivekanandpv/springbootrestfulwebservicesstudy/SpringBootRestfulWebservicesStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekanandpv.springbootrestfulwebservicesstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebservicesStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
