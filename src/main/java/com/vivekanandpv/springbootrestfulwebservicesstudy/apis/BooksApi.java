0333f6729e626ad9b501e72f107c3b995b8284e2
vivek <vivekanandpv@outlook.com>
2023-03-16 07:03:56 +0530

    done

--- src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
@@ -2,68 +2,38 @@ package com.vivekanandpv.springbootrestfulwebservicesstudy.apis;
 
 import com.vivekanandpv.springbootrestfulwebservicesstudy.models.Book;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/books")
 public class BooksApi {
-    //  http://localhost:8080/api/v1/books
-    @GetMapping
-    public ResponseEntity<Book> get() {
+    //  endpoints must have unique urls; query is not the part of url
+    //  http://localhost:8080/api/v1/books/foo?title=Something&pages=458
+
+    @GetMapping("foo")
+    public ResponseEntity<Book> getQueryV1(@RequestParam(required = false) String title, @RequestParam int pages) {
         Book book = new Book();
         book.setId(125);
-        book.setTitle("Learning Spring Boot");
+        book.setTitle(title != null ? title : "Default title");
         book.setAuthors(List.of("John Doe", "Mark David"));
         book.setDescription("Spring Boot for beginners");
-        book.setPages(826);
+        book.setPages(pages);
         book.setPrice(1250.00);
 
         return ResponseEntity.ok(book);
     }
 
-    //  http://localhost:8080/api/v1/books/<id>
-    @GetMapping("{id}")
-    public ResponseEntity<Book> getByIdV1(@PathVariable int id) {
+    @GetMapping("bar")
+    public ResponseEntity<Book> getQueryV2(@RequestParam(required = false) String title, @RequestParam(required = false) Integer pages) {
         Book book = new Book();
-        book.setId(id);
-        book.setTitle("Learning Spring Boot");
+        book.setId(125);
+        book.setTitle(title != null ? title : "Default title");
         book.setAuthors(List.of("John Doe", "Mark David"));
         book.setDescription("Spring Boot for beginners");
-        book.setPages(826);
+        book.setPages(pages != null ? pages : 250);
         book.setPrice(1250.00);
-
-        return ResponseEntity.ok(book);
-    }
-
-    //  http://localhost:8080/api/v1/books/var/<id>
-    @GetMapping("var/{id}")
-    public ResponseEntity<Book> getByIdV2(@PathVariable("id") int varId) {
-        Book book = new Book();
-        book.setId(varId);
-        book.setTitle("Learning Spring Boot");
-        book.setAuthors(List.of("John Doe", "Mark David"));
-        book.setDescription("Spring Boot for beginners");
-        book.setPages(826);
-        book.setPrice(1250.00);
-
-        return ResponseEntity.ok(book);
-    }
-
-    //  http://localhost:8080/api/v1/books/<id>/<price>
-    @GetMapping("{id}/{price}")
-    public ResponseEntity<Book> getByIdV3(@PathVariable int id, @PathVariable double price) {
-        Book book = new Book();
-        book.setId(id);
-        book.setTitle("Learning Spring Boot");
-        book.setAuthors(List.of("John Doe", "Mark David"));
-        book.setDescription("Spring Boot for beginners");
-        book.setPages(826);
-        book.setPrice(price);
 
         return ResponseEntity.ok(book);
     }
