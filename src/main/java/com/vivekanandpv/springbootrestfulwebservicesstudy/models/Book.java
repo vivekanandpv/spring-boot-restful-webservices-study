77d80006553a44a5fa6cca3ec031a1ec38086cbd
vivek <vivekanandpv@outlook.com>
2023-03-16 06:38:46 +0530

    done

---
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/models/Book.java
@@ -0,0 +1,61 @@
+package com.vivekanandpv.springbootrestfulwebservicesstudy.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String description;
+    private List<String> authors = new ArrayList<>();
+    private int pages;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
