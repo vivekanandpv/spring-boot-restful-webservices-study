9bbd68cb9917d7ce5820ace08d83138195bf3957
vivek <vivekanandpv@outlook.com>
2023-03-16 08:30:16 +0530

    done

--- src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
@@ -2,9 +2,7 @@ package com.vivekanandpv.springbootrestfulwebservicesstudy.apis;
 
 import com.vivekanandpv.springbootrestfulwebservicesstudy.models.Book;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,15 +10,34 @@ import java.util.List;
 @RequestMapping("api/v1/books")
 public class BooksApi {
     @GetMapping
-    public ResponseEntity<Book> getSingle() {
-        Book book = new Book();
-        book.setId(125);
-        book.setTitle("Learning Spring Boot");
-        book.setAuthors(List.of("John Doe", "Mark David"));
-        book.setDescription("Spring Boot for beginners");
-        book.setPages(826);
-        book.setPrice(1250.00);
+    public ResponseEntity<?> getSingle() {
+        return ResponseEntity.ok().build();
+    }
 
-        return ResponseEntity.ok(book);
+    @PostMapping
+    public ResponseEntity<?> create() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateWithPut(@PathVariable int id) {
+        return ResponseEntity.ok().build();
+    }
+
+    //  The main difference between the PUT and PATCH method is that
+    //  the PUT method uses the request URI to supply a modified version
+    //  of the requested resource which replaces the original version of
+    //  the resource, whereas the PATCH method supplies a set of instructions
+    //  to modify the resource. If the PATCH document is larger than
+    //  the size of the new version of the resource sent by the PUT method
+    //  then the PUT method is preferred.
+    @PatchMapping("{id}")
+    public ResponseEntity<?> updateWithPatch(@PathVariable int id) {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        return ResponseEntity.ok().build();
     }
 }
