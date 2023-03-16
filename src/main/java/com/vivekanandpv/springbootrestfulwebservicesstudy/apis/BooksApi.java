468ec509dd1fae553b2a719372e5543df65ae71e
vivek <vivekanandpv@outlook.com>
2023-03-16 06:49:03 +0530

    done

--- src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
@@ -11,8 +11,38 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/books")
 public class BooksApi {
+    //  http://localhost:8080/api/v1/books
     @GetMapping
-    public ResponseEntity<Book> getSingle() {
+    public ResponseEntity<Book> getSingleV1() {
+        Book book = new Book();
+        book.setId(125);
+        book.setTitle("Learning Spring Boot");
+        book.setAuthors(List.of("John Doe", "Mark David"));
+        book.setDescription("Spring Boot for beginners");
+        book.setPages(826);
+        book.setPrice(1250.00);
+
+        return ResponseEntity.ok(book);
+    }
+
+    //  http://localhost:8080/api/v1/books/single
+    @GetMapping("single")
+    public ResponseEntity<Book> getSingleV2() {
+        Book book = new Book();
+        book.setId(125);
+        book.setTitle("Learning Spring Boot");
+        book.setAuthors(List.of("John Doe", "Mark David"));
+        book.setDescription("Spring Boot for beginners");
+        book.setPages(826);
+        book.setPrice(1250.00);
+
+        return ResponseEntity.ok(book);
+    }
+
+    //  http://localhost:8080/api/v1/books/foo
+    //  http://localhost:8080/api/v1/books/bar
+    @GetMapping({"foo", "bar"})
+    public ResponseEntity<Book> getSingleV3() {
         Book book = new Book();
         book.setId(125);
         book.setTitle("Learning Spring Boot");
