260f465b9fa4d34c2f7afee463762163413dac3d
vivek <vivekanandpv@outlook.com>
2023-03-16 07:21:20 +0530

    done

--- src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
+++ src/main/java/com/vivekanandpv/springbootrestfulwebservicesstudy/apis/BooksApi.java
@@ -5,34 +5,74 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/books")
 public class BooksApi {
-    //  endpoints must have unique urls; query is not the part of url
-    //  http://localhost:8080/api/v1/books/foo?title=Something&pages=458
-
     @GetMapping("foo")
-    public ResponseEntity<Book> getQueryV1(@RequestParam(required = false) String title, @RequestParam int pages) {
+    public ResponseEntity<Book> getQueryV1(@RequestParam Optional<String> title) {
         Book book = new Book();
         book.setId(125);
-        book.setTitle(title != null ? title : "Default title");
+        book.setTitle(title.orElse("Default title"));
         book.setAuthors(List.of("John Doe", "Mark David"));
         book.setDescription("Spring Boot for beginners");
-        book.setPages(pages);
+        book.setPages(755);
         book.setPrice(1250.00);
 
         return ResponseEntity.ok(book);
     }
 
     @GetMapping("bar")
-    public ResponseEntity<Book> getQueryV2(@RequestParam(required = false) String title, @RequestParam(required = false) Integer pages) {
+    public ResponseEntity<Book> getQueryV2(@RequestParam(name = "title") String varTitle, @RequestParam Optional<Integer> pages) {
         Book book = new Book();
         book.setId(125);
-        book.setTitle(title != null ? title : "Default title");
+        book.setTitle(varTitle != null ? varTitle : "Default title");
         book.setAuthors(List.of("John Doe", "Mark David"));
         book.setDescription("Spring Boot for beginners");
-        book.setPages(pages != null ? pages : 250);
+        book.setPages(pages.orElse(250));
+        book.setPrice(1250.00);
+
+        return ResponseEntity.ok(book);
+    }
+
+    @GetMapping("baz")
+    public ResponseEntity<Book> getQueryV3(@RequestParam Map<String, String> params) {
+        Book book = new Book();
+        book.setId(125);
+        book.setTitle(params.getOrDefault("title", "Default title"));
+        book.setAuthors(List.of("John Doe", "Mark David"));
+        book.setDescription(params.getOrDefault("description", "Default description"));
+        book.setPages(755);
+        book.setPrice(1250.00);
+
+        return ResponseEntity.ok(book);
+    }
+
+    @GetMapping("qux")
+    public ResponseEntity<Book> getQueryV4(@RequestParam(defaultValue = "Default title") String title, @RequestParam Optional<Integer> pages) {
+        Book book = new Book();
+        book.setId(125);
+        book.setTitle(title);
+        book.setAuthors(List.of("John Doe", "Mark David"));
+        book.setDescription("Spring Boot for beginners");
+        book.setPages(pages.orElse(250));
+        book.setPrice(1250.00);
+
+        return ResponseEntity.ok(book);
+    }
+
+    //  ?descriptions=good,book,here
+    @GetMapping("quux")
+    public ResponseEntity<Book> getQueryV5(@RequestParam List<String> descriptions) {
+        Book book = new Book();
+        book.setId(125);
+        book.setTitle("Learning Spring");
+        book.setAuthors(List.of("John Doe", "Mark David"));
+        book.setDescription(String.join(";", descriptions));
+        book.setPages(250);
         book.setPrice(1250.00);
 
         return ResponseEntity.ok(book);
